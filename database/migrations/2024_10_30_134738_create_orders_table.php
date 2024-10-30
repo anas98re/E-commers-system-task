c048cecfa00bf907b4c03d09ead1cbf4075d87ac
anas <o@gamil.com>
2024-10-30 17:00:35 +0300

    Create models and database schema for improved functionality

---
+++ database/migrations/2024_10_30_134738_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+        Schema::dropIfExists('orders');
+    }
+}
